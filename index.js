84f043e3d3f0d92f7b98245df314533374847fa1
Kai Zhang <kaizen.agility@gmail.com>
2022-05-19 06:09:57 -0400

    clean up

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-let jobLeads = []
 let postingsList = [];
 
-
-const deleteBtn = document.getElementById("delete-btn")
-const tabBtn = document.getElementById("save-tab")
-const ulEl = document.getElementById("ul-el")
-const leadsInLocalStorage = JSON.parse(localStorage.getItem("postingsList"))
+const clearBtn = document.getElementById("delete-btn");
+const addBtn = document.getElementById("save-tab");
+const displayList = document.getElementById("displayList");
+const leadsInLocalStorage = JSON.parse(localStorage.getItem("postingsList"));
 
 if(leadsInLocalStorage) {
-
   postingsList = leadsInLocalStorage
   render()
 }
@@ -24,48 +21,35 @@ class Job {
 
 const notes = document.querySelector("#notes");
 const jobstage = document.querySelector("#jobstage");
-const add = document.getElementById("add");
-const clear = document.querySelector("#clear");
-const displayList = document.querySelector("#displayList");
 
-tabBtn.addEventListener("click", () => {
+addBtn.addEventListener("click", () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-  
     let link = tabs[0].url;
-    console.log(tabs[0].url)
     let newJob = new Job();
     newJob.url = link;
     postingsList.push(newJob);
     localStorage.setItem("postingsList", JSON.stringify(postingsList))
     render()
 
-    // let newJob = new Job();
     newJob.notes = notes.value;
     newJob.jobstage = jobstage.value;
     localStorage.setItem("postingsList", JSON.stringify(postingsList))
-    // newJob.link = link;
-    console.log(newJob);
-
   })
 })
-deleteBtn.addEventListener("dblclick", () => {
+clearBtn.addEventListener("dblclick", () => {
   localStorage.clear()
   postingsList = [];
 
   while (displayList.firstChild) {
     displayList.removeChild(displayList.firstChild);
   }
-  // render(jobLeads)
+
   render();
 })
-
-
 
 function render() {
   let listItems = ""
   for(let i = 0; i < postingsList.length; i++) {
-  
-    console.log(postingsList[i].jobstage.value);
     listItems += `
       <h3> ${i+1}.</h3> <br/>
       <p>${postingsList[i].notes}</p></br>
@@ -74,9 +58,6 @@ function render() {
         ${postingsList[i].jobstage}
       </li></br>
       </hr>
-      `
-
-    console.log(postingsList[i].url);
   }
-  ulEl.innerHTML = listItems
+  displayList.innerHTML = listItems
 }
